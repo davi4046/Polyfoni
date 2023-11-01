40571bc9c73b1f341053ea9a4083a729ad14334b
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-11-01 11:08:30 +0100

    Remove shortcuts (broken)

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,6 @@
 import "./styles.css";
 
-import { emit } from "@tauri-apps/api/event";
-import { isRegistered, register } from "@tauri-apps/api/globalShortcut";
-
 import App from "./App.svelte";
-
-if (!(await isRegistered("CmdOrCtrl+I"))) {
-    await register("CmdOrCtrl+I", () => {
-        emit("insert");
-    });
-}
-
-if (!(await isRegistered("Delete"))) {
-    await register("Delete", () => {
-        emit("delete");
-    });
-}
 
 const app = new App({
     target: document.getElementById("app"),
