133879f24eaab27689e74b8fc6407a4d0db04d43
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-11-07 13:55:45 +0100

    Update observer.ts

--- src/lib/observer.ts
+++ src/lib/observer.ts
@@ -1,42 +1,13 @@
 export class Observer {
-    private _initialValues = new Map<Object, Map<PropertyKey, any>>();
-    private _changes = new Map<Object, Map<PropertyKey, any>>();
+    private _changes: [object: Object, property: PropertyKey, newValue: any][] =
+        [];
 
     get changes() {
         return this._changes;
     }
 
-    addObservable(observable: Object, watchProperties: PropertyKey[]) {
-        this._initialValues.set(observable, new Map());
-        this._changes.set(observable, new Map());
-
-        watchProperties.forEach((property) => {
-            let value = observable[property as keyof typeof observable];
-            this._initialValues.get(observable)!.set(property, value);
-        });
-    }
-
     reportChange(target: Object, property: PropertyKey, value: any) {
-        const targetMap = this._changes.get(target);
-
-        if (targetMap) {
-            let initialValue = this._initialValues.get(target)?.get(property);
-
-            if (value == initialValue) {
-                targetMap.delete(property);
-            } else {
-                targetMap.set(property, value);
-            }
-        }
-    }
-
-    flush() {
-        this._changes.forEach((map, object) => {
-            map.forEach((value, property) => {
-                this._initialValues.get(object)?.set(property, value);
-            });
-            this._changes.set(object, new Map());
-        });
+        this._changes.push([target, property, value]);
     }
 }
 
@@ -46,7 +17,13 @@ export abstract class Observable {
         observer: Observer,
         watchProperties: PropertyKey[]
     ) {
-        observer.addObservable(object, watchProperties);
+        watchProperties.forEach((property) => {
+            observer.reportChange(
+                object,
+                property,
+                object[property as keyof typeof object]
+            );
+        });
         return new Proxy(object, {
             deleteProperty: function (target, property) {
                 delete target[property as keyof T];
