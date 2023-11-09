c2fd92c0d4d4a7840bb1c57956f0f63893c0055f
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-11-09 17:42:41 +0100

    Create evaluator script

---
+++ python/evaluator.py
@@ -0,0 +1,26 @@
+import sys
+import math
+import json
+from func_timeout import func_set_timeout, FunctionTimedOut
+
+MATH_DICT = {name: getattr(math, name) for name in dir(math)}
+
+GLOBALS = {"__builtins__": None, **MATH_DICT}
+
+@func_set_timeout(0.1)
+def evaluate(expr, vars):
+    return eval(expr, GLOBALS, vars)
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        try:
+            expr, vars = line.split(", ", 1)
+            vars = json.loads(vars)
+            result = evaluate(expr, vars)
+            
+            sys.stdout.write(str(result) + "\n")
+            sys.stdout.flush()
+        except (Exception, FunctionTimedOut) as e:
+            sys.stderr.write(str(e) + "\n")
+            sys.stderr.flush()
+        
