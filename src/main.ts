15c13b3417bee67bd32fffa22dbfa31b441d04b4
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-11-21 11:51:12 +0100

    Add shortcuts for insert and delete

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import "./styles.css";
 
+import { emit } from "@tauri-apps/api/event";
+import { register, unregister } from "@tauri-apps/api/globalShortcut";
+
 import App from "./App.svelte";
+
+/**
+ * necessary to unregister global shortcuts (before creating app!!!)
+ * for them to work properly during development
+ */
+
+unregister("CmdOrCtrl+I");
+unregister("Delete");
 
 const app = new App({
     target: document.getElementById("app"),
 });
 
 export default app;
+
+//register global shortcuts as a fix for tauri's accelerators not working on Windows
+
+register("CmdOrCtrl+I", (_) => {
+    emit("insert");
+});
+register("Delete", (_) => {
+    emit("delete");
+});
