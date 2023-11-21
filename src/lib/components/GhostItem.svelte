485bf9bcf32172cd67873c2abcf0e032a311217c
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-11-21 11:03:56 +0100

    Fix GhostItem vertical padding

--- src/lib/components/GhostItem.svelte
+++ src/lib/components/GhostItem.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-    class="absolute z-40 h-full py-1 pointer-events-none"
+    class="absolute z-40 h-full py-1.5 pointer-events-none"
     style="width: {(data.end - data.start) * 64}px; left: {data.start * 64}px"
 >
     <div class="border-opacity-75 opacity-75 item">
