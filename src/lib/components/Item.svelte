5720d18a94ddf40e98dcbbcc0d91abfcc3afc380
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-11-21 11:03:37 +0100

    Avoid item being selected when handle is grabbed

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -11,25 +11,27 @@
 </script>
 
 <div class="absolute h-full py-1.5" style="width: {width}px; left: {left}px">
-    <div
-        class="relative z-40 item"
-        title={data.content}
-        class:selected={data.isSelected()}
-        on:mouseenter={(_) => {
-            data.controller?.setHoveredItem(data);
-        }}
-        on:mouseleave={(_) => {
-            data.controller?.setHoveredItem(null);
-        }}
-        role="none"
-    >
-        <p class="truncate">
-            {#if data.content}
-                {data.content}
-            {:else}
-                [empty]
-            {/if}
-        </p>
+    <div class="relative h-full">
+        <div
+            class="relative z-40 item"
+            title={data.content}
+            class:selected={data.isSelected()}
+            on:mouseenter={(_) => {
+                data.controller?.setHoveredItem(data);
+            }}
+            on:mouseleave={(_) => {
+                data.controller?.setHoveredItem(null);
+            }}
+            role="none"
+        >
+            <p class="truncate">
+                {#if data.content}
+                    {data.content}
+                {:else}
+                    [empty]
+                {/if}
+            </p>
+        </div>
         {#if data.startHandle}
             <div class="absolute top-0 bottom-0 left-0 z-50 -translate-x-1/2">
                 <ItemHandle data={data.startHandle} />
