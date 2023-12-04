20b654cab69a592d102d80fcf37703494e2edb1d
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-04 09:46:59 +0100

    Apply formatting to all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{svelte,js,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{svelte,js,ts}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
