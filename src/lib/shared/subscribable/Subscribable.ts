d3c531526da9a51220f5896429716c993d8bf427
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-05 10:43:16 +0100

    Make Subscribable fields readonly

--- src/lib/shared/subscribable/Subscribable.ts
+++ src/lib/shared/subscribable/Subscribable.ts
@@ -3,21 +3,13 @@ type SubscribeCallback<T extends Subscribable> = (value: T) => void;
 class Subscribable {
     private _callbacks: SubscribeCallback<Subscribable>[] = [];
 
-    private _subscribe(callback: SubscribeCallback<Subscribable>) {
+    readonly subscribe = (callback: SubscribeCallback<Subscribable>) => {
         this._callbacks.push(callback);
-    }
+    };
 
-    get subscribe() {
-        return this._subscribe;
-    }
-
-    private _notifySubscribers() {
+    readonly notifySubscribers = () => {
         this._callbacks.forEach((callback) => callback(this));
-    }
-
-    get notifySubscribers() {
-        return this._notifySubscribers;
-    }
+    };
 }
 
 export default Subscribable;
