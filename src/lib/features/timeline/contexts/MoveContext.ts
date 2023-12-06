8e8ab14ec27e062afa4d95bf09303899ce400623
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-06 10:53:28 +0100

    Render items being moved

--- src/lib/features/timeline/contexts/MoveContext.ts
+++ src/lib/features/timeline/contexts/MoveContext.ts
@@ -1,96 +1,134 @@
 import clamp from "../../../shared/utils/math_utils/clamp/clamp";
+import Item from "../models/item/Item";
+import Track from "../models/track/Track";
 import offsetItems from "../utils/offset_items/offsetItems";
 
 import type CursorContext from "./CursorContext";
 import type SelectionContext from "./SelectionContext";
 
 class MoveContext {
+    private _items: Item[] = [];
+
+    get items() {
+        return this._items;
+    }
+
+    private _setItems(newItems: Item[]) {
+        const oldTracks = this._items.map((item) => item.track);
+        const newTracks = newItems.map((item) => item.track);
+        const tracks = Array.from(new Set([...oldTracks, ...newTracks]));
+
+        this._items = newItems;
+
+        tracks.forEach((track) => track.notifySubscribers());
+    }
+
     constructor(
         private _selection: SelectionContext,
         private _cursor: CursorContext
     ) {
+        let lastBeatOffset = 0;
+        let lastTrackOffset = 0;
+        let lastVoiceOffset = 0;
+
         this._cursor.subscribe((_) => {
             const clickedBeat = this._cursor.clickedBeat;
             const clickedTrack = this._cursor.clickedTrack;
             const hoveredBeat = this._cursor.hoveredBeat;
             const hoveredTrack = this._cursor.hoveredTrack;
 
-            if (clickedBeat && clickedTrack && hoveredBeat && hoveredTrack) {
-                const items = this._selection.selectedItems.map((item) => {
-                    return Object.assign({}, item);
-                });
-
-                const minBeat = items.reduce(
-                    (min, item) => Math.min(min, item.start),
-                    Number.MAX_VALUE
-                );
-
-                const beatOffset = Math.max(
-                    hoveredBeat - clickedBeat,
-                    -minBeat
-                );
-
-                const clickedVoice = clickedTrack.voice;
-                const hoveredVoice = hoveredTrack.voice;
-
-                const clickedTrackIndex =
-                    clickedVoice.tracks.indexOf(clickedTrack);
-
-                const hoveredTrackIndex =
-                    hoveredVoice.tracks.indexOf(hoveredTrack);
-
-                const minTrackIndex = items.reduce((min, item) => {
-                    const index = item.track.voice.tracks.indexOf(item.track);
-                    return Math.min(min, index);
-                }, Number.MAX_VALUE);
-
-                const maxTrackIndex = items.reduce((max, item) => {
-                    const index = item.track.voice.tracks.indexOf(item.track);
-                    return Math.max(max, index);
-                }, Number.MIN_VALUE);
-
-                const trackOffset = clamp(
-                    hoveredTrackIndex - clickedTrackIndex,
-                    -minTrackIndex,
-                    5 - maxTrackIndex
-                );
-
-                const clickedVoiceIndex =
-                    clickedVoice.section.voices.indexOf(clickedVoice);
-                const hoveredVoiceIndex =
-                    hoveredVoice.section.voices.indexOf(hoveredVoice);
-
-                const minVoiceIndex = items.reduce((min, item) => {
-                    const index = item.track.voice.section.voices.indexOf(
-                        item.track.voice
-                    );
-                    return Math.min(min, index);
-                }, Number.MAX_VALUE);
-
-                const maxVoiceIndex = items.reduce((max, item) => {
-                    const index = item.track.voice.section.voices.indexOf(
-                        item.track.voice
-                    );
-                    return Math.max(max, index);
-                }, Number.MIN_VALUE);
-
-                const voiceCount = items[0].track.voice.section.voices.length;
-
-                const voiceOffset = clamp(
-                    hoveredVoiceIndex - clickedVoiceIndex,
-                    -minVoiceIndex,
-                    voiceCount - maxVoiceIndex - 1
-                );
-
-                console.log("trackOffset:", trackOffset);
-                console.log("voiceOffset:", voiceOffset);
-
-                offsetItems(items, beatOffset, trackOffset, voiceOffset);
-
-                console.log("moved items:", items);
-            } else {
-                //remove item moves
+            if (
+                !clickedBeat ||
+                !clickedTrack ||
+                !hoveredBeat ||
+                !hoveredTrack
+            ) {
+                this._setItems([]);
+                return;
             }
+
+            const items = this._selection.selectedItems.map((item) => {
+                return Object.assign({}, item);
+            });
+
+            const minBeat = items.reduce(
+                (min, item) => Math.min(min, item.start),
+                Number.MAX_VALUE
+            );
+
+            const beatOffset = Math.max(
+                Math.round(hoveredBeat - clickedBeat),
+                -minBeat
+            );
+
+            const clickedVoice = clickedTrack.voice;
+            const hoveredVoice = hoveredTrack.voice;
+
+            const clickedTrackIndex = clickedVoice.tracks.indexOf(clickedTrack);
+            const hoveredTrackIndex = hoveredVoice.tracks.indexOf(hoveredTrack);
+
+            const minTrackIndex = items.reduce((min, item) => {
+                const index = item.track.voice.tracks.indexOf(item.track);
+                return Math.min(min, index);
+            }, Number.MAX_VALUE);
+
+            const maxTrackIndex = items.reduce((max, item) => {
+                const index = item.track.voice.tracks.indexOf(item.track);
+                return Math.max(max, index);
+            }, Number.MIN_VALUE);
+
+            const trackOffset = clamp(
+                hoveredTrackIndex - clickedTrackIndex,
+                -minTrackIndex,
+                4 - maxTrackIndex
+            );
+
+            const clickedVoiceIndex =
+                clickedVoice.section.voices.indexOf(clickedVoice);
+            const hoveredVoiceIndex =
+                hoveredVoice.section.voices.indexOf(hoveredVoice);
+
+            const minVoiceIndex = items.reduce((min, item) => {
+                const index = item.track.voice.section.voices.indexOf(
+                    item.track.voice
+                );
+                return Math.min(min, index);
+            }, Number.MAX_VALUE);
+
+            const maxVoiceIndex = items.reduce((max, item) => {
+                const index = item.track.voice.section.voices.indexOf(
+                    item.track.voice
+                );
+                return Math.max(max, index);
+            }, Number.MIN_VALUE);
+
+            const voiceCount = items[0].track.voice.section.voices.length;
+
+            const voiceOffset = clamp(
+                hoveredVoiceIndex - clickedVoiceIndex,
+                -minVoiceIndex,
+                voiceCount - maxVoiceIndex - 1
+            );
+
+            if (
+                beatOffset === lastBeatOffset &&
+                trackOffset === lastTrackOffset &&
+                voiceOffset === lastVoiceOffset
+            ) {
+                return;
+            }
+
+            lastBeatOffset = beatOffset;
+            lastTrackOffset = trackOffset;
+            lastVoiceOffset = voiceOffset;
+
+            if (beatOffset === 0 && trackOffset === 0 && voiceOffset === 0) {
+                this._setItems([]);
+                return;
+            }
+
+            offsetItems(items, beatOffset, trackOffset, voiceOffset);
+            this._setItems(items);
         });
     }
 }
--- src/lib/features/timeline/vm_creators/createTrackVM.ts
+++ src/lib/features/timeline/vm_creators/createTrackVM.ts
@@ -1,17 +1,25 @@
 import type TimelineContext from "../contexts/TimelineContext";
 import type Track from "../models/track/Track";
+import type ItemVM from "../view_models/item/ItemVM";
 import TrackVM from "../view_models/track/TrackVM";
 import TrackVMState from "../view_models/track/TrackVMState";
 import createItemVM from "./createItemVM";
 
 function createTrackVM(model: Track, context: TimelineContext): TrackVM {
     const update = (model: Track): TrackVMState => {
-        return new TrackVMState(
-            model.label,
-            model.items.map((item) => {
-                return createItemVM(item, context);
+        const items = model.items.map((item) => {
+            return createItemVM(item, context);
+        });
+
+        const ghostItems = context.move.items
+            .map((item) => {
+                if (item.track === model) {
+                    return createItemVM(item, context);
+                }
             })
-        );
+            .filter((value): value is ItemVM => value !== undefined);
+
+        return new TrackVMState(model.label, [...items, ...ghostItems]);
     };
 
     return new TrackVM(model, update);
