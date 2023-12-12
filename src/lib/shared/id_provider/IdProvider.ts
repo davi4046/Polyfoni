7e2657e832ffb4e71372a3c60545ce428d39a1fb
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-12 13:57:04 +0100

    Introduce IdProvider class

---
+++ src/lib/shared/id_provider/IdProvider.ts
@@ -0,0 +1,14 @@
+import randomId from "../utils/random_id/randomId";
+
+abstract class IdProvider {
+    private static _usedIds: string[] = [];
+
+    static generateId(): string {
+        let id = "";
+        while (id === "" || this._usedIds.includes(id)) id = randomId(10);
+        this._usedIds.push(id);
+        return id;
+    }
+}
+
+export default IdProvider;
--- src/lib/shared/model/Model.ts
+++ src/lib/shared/model/Model.ts
@@ -1,25 +1,15 @@
+import IdProvider from "../id_provider/IdProvider";
 import Stateful from "../stateful/Stateful";
 import Subscribable from "../subscribable/Subscribable";
-import randomId from "../utils/random_id/randomId";
 
 class Model<TState extends object> {
     readonly subscribable = new Subscribable(this);
     readonly state: Stateful<TState> & Required<TState>;
+    readonly id = IdProvider.generateId();
 
     constructor(state: (model: Model<TState>) => Required<TState>) {
         this.state = Stateful.create(state(this));
     }
-
-    private static _usedIds: string[] = [];
-
-    private static _uniqueId(): string {
-        let id = "";
-        while (id === "" || this._usedIds.includes(id)) id = randomId(10);
-        Model._usedIds.push(id);
-        return id;
-    }
-
-    readonly id = Model._uniqueId();
 }
 
 export default Model;
