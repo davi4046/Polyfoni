0ca557cede1a3740163baad8a4b5d75080598a2c
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-14 09:08:24 +0100

    Rename _state to _stateful

--- src/lib/shared/view_model/ViewModel.ts
+++ src/lib/shared/view_model/ViewModel.ts
@@ -4,27 +4,23 @@ import Subscribable from "../subscribable/Subscribable";
 
 class ViewModel<TModel extends Model<any>, TState extends object> {
     private _subscribable = new Subscribable(this);
-    private _state: Stateful<TState> & Required<TState>;
+    private _stateful: Stateful<TState> & Required<TState>;
     readonly modelId: string;
 
     constructor(model: TModel, update: (model: TModel) => Required<TState>) {
-        this._state = Stateful.create(update(model));
+        this._stateful = Stateful.create(update(model));
         this.modelId = model.id;
 
         model.subscribe((_) => {
-            this._state.setState(update(model));
+            this._stateful.setState(update(model));
             this._subscribable.notifySubscribers();
         });
 
         this._subscribable.notifySubscribers();
     }
 
-    set state(newState: Partial<TState>) {
-        this._state.setState(newState);
-    }
-
     get state(): Required<TState> {
-        return this._state.getState() as Required<TState>;
+        return this._stateful.getState() as Required<TState>;
     }
 
     subscribe(callback: (value: this) => void) {
