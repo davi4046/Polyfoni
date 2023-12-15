415fb362efe0d35135e9c87224a1b7b4e60ff49c
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-15 14:46:09 +0100

    Remove mapCoordsToTimeline method

--- src/lib/features/timeline/behaviours/timeline_drag/TimelineDrag.ts
+++ src/lib/features/timeline/behaviours/timeline_drag/TimelineDrag.ts
@@ -12,27 +12,6 @@ abstract class TimelineDrag implements DragBehaviour {
     private _prevFromTrack: Track | null = null;
     private _prevToTrack: Track | null = null;
 
-    private mapCoordsToTimeline(
-        fromX: number,
-        toX: number,
-        fromY: number,
-        toY: number
-    ): {
-        fromBeat: number;
-        toBeat: number;
-        fromTrack: Track | null;
-        toTrack: Track | null;
-    } {
-        const fromBeat = Math.round(
-            getBeatAtClientX(this.context.timeline, fromX)
-        );
-        const toBeat = Math.round(getBeatAtClientX(this.context.timeline, toX));
-        const fromTrack = findClosestTrack(this.context.timeline, fromY);
-        const toTrack = findClosestTrack(this.context.timeline, toY); // TODO: should round up when dragging down and round down when dragging up
-
-        return { fromBeat, toBeat, fromTrack, toTrack };
-    }
-
     protected abstract handleDrag(
         fromBeat: number,
         toBeat: number,
@@ -43,8 +22,12 @@ abstract class TimelineDrag implements DragBehaviour {
     protected abstract handleDrop(): void;
 
     drag(fromX: number, toX: number, fromY: number, toY: number) {
-        const { fromBeat, toBeat, fromTrack, toTrack } =
-            this.mapCoordsToTimeline(fromX, toX, fromY, toY);
+        const fromBeat = Math.round(
+            getBeatAtClientX(this.context.timeline, fromX)
+        );
+        const toBeat = Math.round(getBeatAtClientX(this.context.timeline, toX));
+        const fromTrack = findClosestTrack(this.context.timeline, fromY);
+        const toTrack = findClosestTrack(this.context.timeline, toY); // TODO: should round up when dragging down and round down when dragging up
 
         if (!fromTrack || !toTrack) return;
 
