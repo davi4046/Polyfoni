c7829e66e2f3937ba881767648ff84003819a9b1
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-15 13:53:25 +0100

    Extract mapCoordsToTimeline method

--- src/lib/features/timeline/behaviours/timeline_drag/TimelineDrag.ts
+++ src/lib/features/timeline/behaviours/timeline_drag/TimelineDrag.ts
@@ -12,6 +12,27 @@ abstract class TimelineDrag implements DragBehaviour {
     private _prevFromTrack: Track | null = null;
     private _prevToTrack: Track | null = null;
 
+    private mapCoordsToTimeline(
+        fromX: number,
+        toX: number,
+        fromY: number,
+        toY: number
+    ): {
+        fromBeat: number;
+        toBeat: number;
+        fromTrack: Track | null;
+        toTrack: Track | null;
+    } {
+        const fromBeat = Math.round(
+            getBeatAtClientX(this.context.timeline, fromX)
+        );
+        const toBeat = Math.round(getBeatAtClientX(this.context.timeline, toX));
+        const fromTrack = findClosestTrack(this.context.timeline, fromY);
+        const toTrack = findClosestTrack(this.context.timeline, toY); // TODO: should round up when dragging down and round down when dragging up
+
+        return { fromBeat, toBeat, fromTrack, toTrack };
+    }
+
     protected abstract handleDrag(
         fromBeat: number,
         toBeat: number,
@@ -21,19 +42,9 @@ abstract class TimelineDrag implements DragBehaviour {
 
     protected abstract handleDrop(): void;
 
-    readonly drag = (
-        fromX: number,
-        toX: number,
-        fromY: number,
-        toY: number
-    ) => {
-        const fromBeat = Math.round(
-            getBeatAtClientX(this.context.timeline, fromX)
-        );
-        const toBeat = Math.round(getBeatAtClientX(this.context.timeline, toX));
-
-        const fromTrack = findClosestTrack(this.context.timeline, fromY);
-        const toTrack = findClosestTrack(this.context.timeline, toY); // TODO: should round up when dragging down and round down when dragging up
+    drag(fromX: number, toX: number, fromY: number, toY: number) {
+        const { fromBeat, toBeat, fromTrack, toTrack } =
+            this.mapCoordsToTimeline(fromX, toX, fromY, toY);
 
         if (!fromTrack || !toTrack) return;
 
@@ -52,9 +63,11 @@ abstract class TimelineDrag implements DragBehaviour {
         this._prevToTrack = toTrack;
 
         this.handleDrag(fromBeat, toBeat, fromTrack, toTrack);
-    };
+    }
 
-    readonly drop = () => this.handleDrop();
+    drop() {
+        this.handleDrop();
+    }
 }
 
 export default TimelineDrag;
