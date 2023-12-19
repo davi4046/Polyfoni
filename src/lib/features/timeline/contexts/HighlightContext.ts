a524393c72ca017ac3a2801c4c55ab760ead6bbf
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-19 14:57:47 +0100

    Set highlights when dragging on timeline

--- src/lib/features/timeline/contexts/HighlightContext.ts
+++ src/lib/features/timeline/contexts/HighlightContext.ts
@@ -1,3 +1,14 @@
-class HighlightContext {}
+import type Track from "../models/track/Track";
+
+type Highlight = { track: Track; start: number; end: number };
+
+class HighlightContext {
+    private _highlights: Highlight[] = [];
+
+    set highlights(newHighlights: Highlight[]) {
+        this._highlights = newHighlights;
+        console.log(newHighlights);
+    }
+}
 
 export default HighlightContext;
--- src/lib/features/timeline/mouse_event_handlers/TimelineHandler.ts
+++ src/lib/features/timeline/mouse_event_handlers/TimelineHandler.ts
@@ -1,5 +1,8 @@
-import type TimelineContext from "../contexts/TimelineContext";
+import findClosestTrack from "../utils/find_closest_track.ts/findClosestTrack";
+import getBeatAtClientX from "../utils/get_beat_at_client_x/getBeatAtClientX";
+import getTracksInRange from "../utils/get_tracks_in_range/getTracksInRange";
 
+import type TimelineContext from "../contexts/TimelineContext";
 class TimelineHandler implements MouseEventHandler {
     constructor(readonly context: TimelineContext) {}
 
@@ -8,7 +11,40 @@ class TimelineHandler implements MouseEventHandler {
         this.context.selection.deselectAll();
     }
 
-    handleMouseMove(moveEvent: MouseEvent, downEvent?: MouseEvent) {}
+    handleMouseMove(moveEvent: MouseEvent, downEvent?: MouseEvent) {
+        if (!downEvent) return;
+
+        const hoveredBeat = Math.round(
+            getBeatAtClientX(this.context.timeline, moveEvent.clientX)
+        );
+        const clickedBeat = Math.round(
+            getBeatAtClientX(this.context.timeline, downEvent.clientX)
+        );
+
+        const hoveredTrack = findClosestTrack(
+            this.context.timeline,
+            moveEvent.clientY
+        );
+        const clickedTrack = findClosestTrack(
+            this.context.timeline,
+            downEvent.clientY
+        );
+
+        if (
+            !hoveredTrack ||
+            !clickedTrack ||
+            (hoveredBeat === clickedBeat && hoveredTrack === clickedTrack)
+        ) {
+            return;
+        }
+
+        this.context.highlight.highlights = getTracksInRange(
+            clickedTrack,
+            hoveredTrack
+        ).map((track) => {
+            return { track: track, start: clickedBeat, end: hoveredBeat };
+        });
+    }
 
     handleMouseUp(upEvent: MouseEvent, downEvent: MouseEvent) {}
 }
