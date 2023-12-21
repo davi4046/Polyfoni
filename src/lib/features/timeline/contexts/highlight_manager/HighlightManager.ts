e1fe2059bd6ba772d29a7c99933757868b4274d9
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-21 10:44:23 +0100

    Create basic rendering of highlights

--- src/lib/features/timeline/contexts/highlight_manager/HighlightManager.ts
+++ src/lib/features/timeline/contexts/highlight_manager/HighlightManager.ts
@@ -1,23 +1,54 @@
+import { makeHull } from "../../../../shared/point/utils/convexHull";
+import createSvgPath from "../../../../shared/point/utils/createSvgPath";
+import Path from "../../views/components/Path.svelte";
+import getCornerPoints from "./utils/getCornerPoints";
+import groupByIndex from "./utils/groupByIndex";
+import groupByVoice from "./utils/groupByVoice";
+
 import type Highlight from "./utils/Highlight";
 
 class HighlightManager {
     private _highlights: Highlight[] = [];
 
+    private _paths: Path[] = [];
+
     set highlights(newHighlights: Highlight[]) {
         this._highlights = newHighlights;
+        this.renderHighlights();
     }
 
     addHighlight(newHighlight: Highlight) {
         const highlight = this._highlights.find(
             (highlight) => highlight.track === newHighlight.track
         );
-
         if (highlight) {
             highlight.start = Math.min(highlight.start, newHighlight.start);
             highlight.end = Math.max(highlight.end, newHighlight.end);
         } else {
             this._highlights.push(newHighlight);
         }
+        this.renderHighlights();
+    }
+
+    renderHighlights() {
+        const groups = groupByVoice(this._highlights).map(groupByIndex).flat();
+
+        for (const path of this._paths) {
+            path.$destroy();
+        }
+
+        this._paths = groups.map((group) => {
+            const points = group.map(getCornerPoints).flat();
+            const convexHull = makeHull(points);
+            const appElement = document.getElementById("app")!;
+
+            return new Path({
+                target: appElement,
+                props: {
+                    path: createSvgPath(convexHull),
+                },
+            });
+        });
     }
 }
 
---
+++ src/lib/features/timeline/views/components/Path.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let path: string;
+</script>
+
+<svg
+    class="absolute overflow-visible"
+    shape-rendering="crispEdges"
+    style="
+    left: 0px; 
+    top: 0px;
+    "
+>
+    <path d={path} fill="none" stroke="blue" stroke-width="4px" />
+</svg>
