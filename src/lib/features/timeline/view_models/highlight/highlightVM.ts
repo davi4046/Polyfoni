5ae2f05b5bfa096c73ea4004140b19abdc5d3874
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-23 01:54:18 +0100

    Delete old highlight components

--- src/lib/features/timeline/view_models/highlight/highlightVM.ts
+++
@@ -1,7 +0,0 @@
-import Model from "../../../../shared/model/Model";
-
-import type { HighlightVMState } from "./highlightVMState";
-
-class HighlightVM extends Model<HighlightVMState> {}
-
-export default HighlightVM;
