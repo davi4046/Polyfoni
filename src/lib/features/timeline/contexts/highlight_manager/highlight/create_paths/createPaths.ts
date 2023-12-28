cfc72fef9ce19b37f40ff2a64e330c618511fb8a
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-28 11:37:55 +0100

    Delete groupBySection.ts

--- src/lib/features/timeline/contexts/highlight_manager/highlight/create_paths/createPaths.ts
+++ src/lib/features/timeline/contexts/highlight_manager/highlight/create_paths/createPaths.ts
@@ -2,7 +2,6 @@ import type Point from "../../../../../../shared/utils/point/Point";
 import polygonClipping from "polygon-clipping";
 
 import { getIndex } from "../../../../../../shared/architecture/state/state_utils";
-import groupBySection from "../group_by_section/groupBySection";
 import groupByTrack from "../group_by_track/groupByTrack";
 import groupByVoice from "../group_by_voice/groupByVoice";
 import getRect from "./rectangle/get_rect/getRect";
@@ -33,43 +32,39 @@ function getVerticalFiller(
 }
 
 function createPaths(highlights: Highlight[]): Point[][] {
-    const groups = groupBySection(highlights).map((group) =>
-        groupByVoice(group).map((group) =>
-            groupByTrack(group).sort(
-                // sort track groups by track index
-                (a, b) => getIndex(a[0].track) - getIndex(b[0].track)
-            )
+    const groups = groupByVoice(highlights).map((group) =>
+        groupByTrack(group).sort(
+            // sort track groups by track index
+            (a, b) => getIndex(a[0].track) - getIndex(b[0].track)
         )
     );
 
     const rects: Rectangle[] = [];
 
-    for (const sectionGroup of groups) {
-        for (const voiceGroup of sectionGroup) {
-            for (let i = 0; i < voiceGroup.length; i++) {
-                const currTrackGroup = voiceGroup[i];
-                const currTrackRects = currTrackGroup.map(getRect);
+    for (const voiceGroup of groups) {
+        for (let i = 0; i < voiceGroup.length; i++) {
+            const currTrackGroup = voiceGroup[i];
+            const currTrackRects = currTrackGroup.map(getRect);
 
-                currTrackRects.forEach((rect) => rects.push(rect));
+            currTrackRects.forEach((rect) => rects.push(rect));
 
-                if (i === voiceGroup.length - 1) continue;
+            if (i === voiceGroup.length - 1) continue;
 
-                const nextTrackGroup = voiceGroup[i + 1];
+            const nextTrackGroup = voiceGroup[i + 1];
 
-                if (
-                    getIndex(nextTrackGroup[0].track) !==
-                    getIndex(currTrackGroup[0].track) + 1
-                ) {
-                    continue;
-                }
+            if (
+                getIndex(nextTrackGroup[0].track) !==
+                getIndex(currTrackGroup[0].track) + 1
+            ) {
+                continue;
+            }
 
-                const nextTrackRects = nextTrackGroup.map(getRect);
+            const nextTrackRects = nextTrackGroup.map(getRect);
 
-                for (const upper of currTrackRects) {
-                    for (const lower of nextTrackRects) {
-                        const filler = getVerticalFiller(upper, lower);
-                        if (filler) rects.push(filler);
-                    }
+            for (const upper of currTrackRects) {
+                for (const lower of nextTrackRects) {
+                    const filler = getVerticalFiller(upper, lower);
+                    if (filler) rects.push(filler);
                 }
             }
         }
