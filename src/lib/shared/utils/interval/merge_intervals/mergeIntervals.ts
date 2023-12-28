6f2473f6f60721a7edeb5f40e76e8a9fcaa05a61
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-12-28 09:47:01 +0100

    Create mergeIntervals.ts

---
+++ src/lib/shared/utils/interval/merge_intervals/mergeIntervals.ts
@@ -0,0 +1,32 @@
+import type Interval from "../Interval";
+
+function mergeIntervals(intervals: Interval[]): Interval[] {
+    if (intervals.length <= 1) {
+        return intervals.slice(); // Return a copy of the input array
+    }
+
+    // Sort intervals based on their start values
+    intervals.sort((a, b) => a.start - b.start);
+
+    const mergedIntervals: Interval[] = [intervals[0]];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const currentInterval = intervals[i];
+        const lastMergedInterval = mergedIntervals[mergedIntervals.length - 1];
+
+        if (currentInterval.start <= lastMergedInterval.end) {
+            // Overlapping intervals, merge them
+            lastMergedInterval.end = Math.max(
+                lastMergedInterval.end,
+                currentInterval.end
+            );
+        } else {
+            // Non-overlapping interval, add a new interval to the result
+            mergedIntervals.push({ ...currentInterval });
+        }
+    }
+
+    return mergedIntervals;
+}
+
+export default mergeIntervals;
