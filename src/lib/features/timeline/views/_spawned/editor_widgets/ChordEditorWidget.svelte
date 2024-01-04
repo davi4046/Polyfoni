04adfe04a9075d4f9f8796e3622ac61c24a0e830
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-01-04 10:46:47 +0100

    Move editor_widgets directory

--- src/lib/features/timeline/views/_spawned/editor_widgets/ChordEditorWidget.svelte
+++ src/lib/features/timeline/views/_spawned/editor_widgets/ChordEditorWidget.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import type Chord from "../../utils/chord/Chord";
+    import type Chord from "../../../utils/chord/Chord";
 
     export let update: (value: Chord) => void;
 </script>
