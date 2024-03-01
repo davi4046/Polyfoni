9302c4172d41ee50007d6cdf294fb83a8fc854d3
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-01 15:31:31 +0100

    Fix test.ts

--- src/lib/shared/utils/interval/clear_interval/test.ts
+++ src/lib/shared/utils/interval/clear_interval/test.ts
@@ -1,55 +1,62 @@
-import Interval from "../Interval";
+import type Interval from "../Interval";
+
 import clearInterval from "./clearInterval";
 
 describe("clearInterval", () => {
     let list: Interval[];
 
     beforeEach(() => {
-        list = [new Interval(4, 8)];
+        list = [{ start: 4, end: 8 }];
     });
 
     test("crops overlapping interval with lesser start, lesser end", () => {
-        clearInterval(list, new Interval(2, 6));
-        expect(list).toEqual([new Interval(6, 8)]);
+        clearInterval(list, { start: 2, end: 6 });
+        expect(list).toEqual([{ start: 6, end: 8 }]);
     });
 
     test("splits overlapping interval with lesser start, greater end", () => {
-        clearInterval(list, new Interval(5, 7));
-        expect(list).toEqual([new Interval(4, 5), new Interval(7, 8)]);
+        clearInterval(list, { start: 5, end: 7 });
+        expect(list).toEqual([
+            { start: 4, end: 5 },
+            { start: 7, end: 8 },
+        ]);
     });
 
     test("crops overlapping interval with lesser start, equal end", () => {
-        clearInterval(list, new Interval(5, 7));
-        expect(list).toEqual([new Interval(4, 5), new Interval(7, 8)]);
+        clearInterval(list, { start: 5, end: 7 });
+        expect(list).toEqual([
+            { start: 4, end: 5 },
+            { start: 7, end: 8 },
+        ]);
     });
 
     test("removes overlapping interval with greater start, lesser end", () => {
-        clearInterval(list, new Interval(2, 10));
+        clearInterval(list, { start: 2, end: 8 });
         expect(list).toEqual([]);
     });
 
     test("crops overlapping interval with greater start, greater end", () => {
-        clearInterval(list, new Interval(2, 6));
-        expect(list).toEqual([new Interval(6, 8)]);
+        clearInterval(list, { start: 2, end: 6 });
+        expect(list).toEqual([{ start: 6, end: 8 }]);
     });
 
     test("removes overlapping interval with greater start, equal end", () => {
-        clearInterval(list, new Interval(2, 8));
+        clearInterval(list, { start: 2, end: 8 });
         expect(list).toEqual([]);
     });
 
     test("removes overlapping interval with equal start, lesser end", () => {
-        clearInterval(list, new Interval(4, 10));
+        clearInterval(list, { start: 4, end: 10 });
         expect(list).toEqual([]);
     });
 
     test("crops overlapping interval with equal start, greater end", () => {
-        clearInterval(list, new Interval(4, 6));
-        expect(list).toEqual([new Interval(6, 8)]);
+        clearInterval(list, { start: 4, end: 6 });
+        expect(list).toEqual([{ start: 6, end: 8 }]);
     });
 
     test("removes interval with equal start, equal end", () => {
-        clearInterval(list, new Interval(4, 8));
+        clearInterval(list, { start: 4, end: 8 });
         expect(list).toEqual([]);
     });
 });
