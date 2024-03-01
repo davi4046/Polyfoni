20807096e94c15fef3ee2b0bace828a575c8665f
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-01 07:54:03 +0100

    Turn related functions into methods of Chord

--- src/lib/features/timeline/_shared/chord/Chord.ts
+++ src/lib/features/timeline/_shared/chord/Chord.ts
@@ -1,17 +1,35 @@
-import degreeToPitch from "./degreeToPitch";
-import getBinary from "./getBinary";
-import getDecimal from "./getDecimal";
-
-class Chord {
+export default class Chord {
     readonly pitchClassSet: number[];
 
     constructor(pitches: number[]) {
         this.pitchClassSet = Array.from(new Set(pitches));
     }
 
-    degreeToPitch = degreeToPitch;
-    getBinary = getBinary;
-    getDecimal = getDecimal;
-}
+    degreeToPitch(degree: number): number {
+        let octave = Math.floor(degree / this.pitchClassSet.length) + 4;
+        let index = degree % this.pitchClassSet.length;
+        while (index < 0) index += this.pitchClassSet.length;
+        return this.pitchClassSet[index] + 12 * octave;
+    }
 
-export default Chord;
+    getBinary(this: Chord): string {
+        let root = this.pitchClassSet[0] % 12;
+
+        let binary = Array(12).fill("0");
+
+        this.pitchClassSet.forEach((pitchClass) => {
+            let index = pitchClass - root;
+
+            while (index >= 12) index -= 12;
+            while (index < 0) index += 12;
+
+            binary[11 - index] = "1";
+        });
+
+        return binary.join("");
+    }
+
+    getDecimal(this: Chord): number {
+        return parseInt(this.getBinary(), 2);
+    }
+}
--- src/lib/features/timeline/_shared/chord/getDecimal.ts
+++
@@ -1,7 +0,0 @@
-import type Chord from "./Chord";
-
-function getDecimal(this: Chord): number {
-    return parseInt(this.getBinary(), 2);
-}
-
-export default getDecimal;
