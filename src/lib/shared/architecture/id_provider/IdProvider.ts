e458bcf1e8e2ed208902b01fdee6ba3e43512596
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-01 13:16:09 +0100

    Improve IdProvider

--- src/lib/shared/architecture/id_provider/IdProvider.ts
+++ src/lib/shared/architecture/id_provider/IdProvider.ts
@@ -1,14 +1,12 @@
 import randomId from "./random_id/randomId";
 
-abstract class IdProvider {
-    private static _usedIds: string[] = [];
+export default class IdProvider {
+    private _usedIds: string[] = [];
 
-    static generateId(): string {
+    generateId(): string {
         let id = "";
         while (id === "" || this._usedIds.includes(id)) id = randomId(10);
         this._usedIds.push(id);
         return id;
     }
 }
-
-export default IdProvider;
--- src/lib/shared/architecture/model/Model.ts
+++ src/lib/shared/architecture/model/Model.ts
@@ -2,10 +2,12 @@ import IdProvider from "../id_provider/IdProvider";
 
 import Stateful from "./Stateful";
 
+const modelIdProvider = new IdProvider();
+
 export default class Model<TState extends object> extends Stateful<TState> {
     constructor(
         state: TState,
-        readonly id = IdProvider.generateId()
+        readonly id = modelIdProvider.generateId()
     ) {
         super(state);
     }
