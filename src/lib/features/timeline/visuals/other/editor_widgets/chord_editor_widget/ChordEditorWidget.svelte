dae910389c9828e5cc6dc48453fdd4c77c3a516d
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-05 13:41:41 +0100

    Create button layout

--- src/lib/features/timeline/visuals/other/editor_widgets/chord_editor_widget/ChordEditorWidget.svelte
+++ src/lib/features/timeline/visuals/other/editor_widgets/chord_editor_widget/ChordEditorWidget.svelte
@@ -3,6 +3,7 @@
     import pitchNames from "../../../../utils/pitchNames";
     import RotateLeftIcon from "./assets/RotateLeftIcon.svelte";
     import RotateRightIcon from "./assets/RotateRightIcon.svelte";
+    import SpeakerIcon from "./assets/SpeakerIcon.svelte";
 
     export let value: ChordBuilder;
     export let update: (value: ChordBuilder) => void;
@@ -17,39 +18,75 @@
     ];
 </script>
 
-<div
-    class="grid auto-cols-min grid-flow-col grid-rows-[min-content,auto] gap-x-4 p-2"
->
-    <div class="text-sm font-medium">Root</div>
-    <select
-        class="w-24 p-2 text-xl font-medium bg-gray-200"
-        title="Root"
-        bind:value={value.root}
+<div class="flex">
+    <div
+        class="grid auto-cols-min grid-flow-col grid-rows-[min-content,auto] gap-x-4 p-2"
     >
-        <option>---</option>
-        {#each pitchNames as pitch}
-            <option>{pitch}</option>
-        {/each}
-    </select>
-    <div class="text-sm font-medium">Decimal</div>
-    <input
-        class="w-24 p-2 text-xl font-medium bg-gray-200"
-        type="number"
-        title="Decimal"
-        bind:value={value.decimal}
-    />
-    <div class="text-sm font-medium">Pitches</div>
-    <div class="grid grid-flow-col gap-1 auto-cols-min">
-        {#each sortedPitchEntries as [pitch, isChecked]}
-            <button
-                class="h-12 w-12 rounded-full text-xl font-medium hover:brightness-105 {isChecked
-                    ? 'bg-gray-300'
-                    : 'opacity-50'}"
-                on:click={(_) => {
-                    // @ts-ignore
-                    value.pitches[pitch] = !isChecked;
-                }}>{pitch}</button
-            >
-        {/each}
+        <div class="text-sm font-medium">Root</div>
+        <select
+            class="w-24 p-2 text-xl font-medium bg-gray-200"
+            title="Root"
+            bind:value={value.root}
+        >
+            <option>---</option>
+            {#each pitchNames as pitch}
+                <option>{pitch}</option>
+            {/each}
+        </select>
+        <div class="text-sm font-medium">Decimal</div>
+        <input
+            class="w-24 p-2 text-xl font-medium bg-gray-200"
+            type="number"
+            title="Decimal"
+            bind:value={value.decimal}
+        />
+        <div class="text-sm font-medium">Pitches</div>
+        <div class="grid grid-flow-col gap-1 auto-cols-min place-items-center">
+            {#each sortedPitchEntries as [pitch, isChecked]}
+                <button
+                    class="h-12 w-12 rounded-full text-xl font-medium hover:brightness-105 {isChecked
+                        ? 'bg-gray-300'
+                        : 'opacity-50'}"
+                    on:click={(_) => {
+                        // @ts-ignore
+                        value.pitches[pitch] = !isChecked;
+                    }}>{pitch}</button
+                >
+            {/each}
+        </div>
+    </div>
+    <div class="grid grid-flow-col grid-rows-2 gap-2 p-2 auto-cols-fr">
+        <button
+            class="btn-default flex place-items-center space-x-0.5 p-1 font-medium"
+        >
+            <div class="h-5">
+                <RotateLeftIcon />
+            </div>
+            <div>Rotate Left</div>
+        </button>
+        <button
+            class="btn-default flex place-items-center space-x-0.5 p-1 font-medium"
+        >
+            <div class="h-5">
+                <RotateRightIcon />
+            </div>
+            <div>Rotate Right</div>
+        </button>
+        <button
+            class="btn-default flex place-items-center space-x-0.5 p-1 font-medium"
+        >
+            <div class="h-5">
+                <SpeakerIcon />
+            </div>
+            <div>Listen Chord</div>
+        </button>
+        <button
+            class="btn-default flex place-items-center space-x-0.5 p-1 font-medium"
+        >
+            <div class="h-5">
+                <SpeakerIcon />
+            </div>
+            <div>Listen Scale</div>
+        </button>
     </div>
 </div>
