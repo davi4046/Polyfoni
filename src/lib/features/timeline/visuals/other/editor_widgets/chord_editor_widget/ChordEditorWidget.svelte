16d339f74d99e9dcf9932b97a974b466bad827cd
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-05 13:01:19 +0100

    Update layout

--- src/lib/features/timeline/visuals/other/editor_widgets/chord_editor_widget/ChordEditorWidget.svelte
+++ src/lib/features/timeline/visuals/other/editor_widgets/chord_editor_widget/ChordEditorWidget.svelte
@@ -17,75 +17,39 @@
     ];
 </script>
 
-<div class="flex p-2 space-x-4">
-    <div>
-        <p class="font-medium">Filter</p>
-        <div class="flex border-2 border-gray-400 rounded-sm w-min">
-            <input
-                class="w-16 pl-1 border-r-2 border-gray-400"
-                type="text"
-                bind:value={filter}
-            />
-            <select class="w-4" bind:value={filter}>
-                <option>None</option>
-                <option>A-2743</option>
-                <option>A-1243</option>
-                <option>A-545</option>
-            </select>
-        </div>
-    </div>
-    <div>
-        <p class="font-medium">Root</p>
-        <select
-            class="border-2 border-gray-400 rounded-sm"
-            bind:value={value.root}
-        >
-            {#each pitchNames as pitch}
-                <option>{pitch}</option>
-            {/each}
-        </select>
-    </div>
-    <div>
-        <p class="font-medium">Decimal</p>
-        <input
-            class="pl-1 border-2 border-gray-400"
-            type="number"
-            bind:value={value.decimal}
-        />
-    </div>
-    <div class="grid grid-flow-col gap-1 auto-cols-fr">
+<div
+    class="grid auto-cols-min grid-flow-col grid-rows-[min-content,auto] gap-x-4 p-2"
+>
+    <div class="text-sm font-medium">Root</div>
+    <select
+        class="w-24 p-2 text-xl font-medium bg-gray-200"
+        title="Root"
+        bind:value={value.root}
+    >
+        <option>---</option>
+        {#each pitchNames as pitch}
+            <option>{pitch}</option>
+        {/each}
+    </select>
+    <div class="text-sm font-medium">Decimal</div>
+    <input
+        class="w-24 p-2 text-xl font-medium bg-gray-200"
+        type="number"
+        title="Decimal"
+        bind:value={value.decimal}
+    />
+    <div class="text-sm font-medium">Pitches</div>
+    <div class="grid grid-flow-col gap-1 auto-cols-min">
         {#each sortedPitchEntries as [pitch, isChecked]}
             <button
-                class="h-12 w-12 rounded-full border-black text-xl font-medium hover:bg-gray-200 {isChecked
-                    ? 'border-2'
-                    : ''}"
+                class="h-12 w-12 rounded-full text-xl font-medium hover:brightness-105 {isChecked
+                    ? 'bg-gray-300'
+                    : 'opacity-50'}"
                 on:click={(_) => {
                     // @ts-ignore
                     value.pitches[pitch] = !isChecked;
                 }}>{pitch}</button
             >
         {/each}
-    </div>
-    <div class="flex space-x-2">
-        <button
-            class="h-12 text-gray-500 hover:text-gray-600"
-            title="Rotate Right"
-            on:click={(_) => {
-                value.rotate("R");
-                value = value; // Reactivity hack, do not remove
-            }}
-        >
-            <RotateRightIcon />
-        </button>
-        <button
-            class="h-12 text-gray-500 hover:text-gray-600"
-            title="Rotate Left"
-            on:click={(_) => {
-                value.rotate("L");
-                value = value; // Reactivity hack, do not remove
-            }}
-        >
-            <RotateLeftIcon />
-        </button>
     </div>
 </div>
