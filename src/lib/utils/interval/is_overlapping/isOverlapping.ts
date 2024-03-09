dd89df64e30cf65aee60eeaaac308b70d92f81eb
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-09 17:17:18 +0100

    Improve isOverlapping function

--- src/lib/utils/interval/is_overlapping/isOverlapping.ts
+++ src/lib/utils/interval/is_overlapping/isOverlapping.ts
@@ -2,7 +2,8 @@ import type Interval from "../Interval";
 
 export default function isOverlapping(a: Interval, b: Interval): Boolean {
     return (
-        (a.start >= b.start && a.start < b.end) ||
-        (a.end > b.start && a.end <= b.end)
+        (a.start >= b.start && a.start < b.end) || // Start of A is within B
+        (a.end > b.start && a.end <= b.end) || // End of A is within B
+        (a.start <= b.start && a.end >= b.end) // A contains B
     );
 }
