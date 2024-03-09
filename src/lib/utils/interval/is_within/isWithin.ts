2b5eafdd42e11d5def04fd796f656122832f0444
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-09 15:57:03 +0100

    Convert named export to export modifier

--- src/lib/utils/interval/is_within/isWithin.ts
+++ src/lib/utils/interval/is_within/isWithin.ts
@@ -3,8 +3,6 @@ import type Interval from "../Interval";
 /**
  * @returns true if the value is within the interval, otherwise false
  */
-function isWithin(value: number, interval: Interval): Boolean {
+export default function isWithin(value: number, interval: Interval): Boolean {
     return value >= interval.start && value <= interval.end;
 }
-
-export default isWithin;
