ddbceb53f72b49eb207407c602d46239a9ff049f
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-11 19:21:31 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import "./styles.css";
 import "./lib/utils/html_classes/adjust-width-to-height";
 
 const app = new App({
-    target: document.getElementById("app"),
+    target: document.getElementById("app")!,
 });
 
 export default app;
