4ae9b5d1e5521f729b67fba81c6382cbf170f694
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-14 15:09:59 +0100

    Rename createTrackVM.ts to createItemTrackVM.ts

--- src/lib/features/timeline/vm_creators/createVoiceVM.ts
+++ src/lib/features/timeline/vm_creators/createVoiceVM.ts
@@ -2,7 +2,7 @@ import type TimelineContext from "../context/TimelineContext";
 import type Voice from "../models/Voice";
 import VoiceVM from "../view_models/VoiceVM";
 
-import createTrackVM from "./createTrackVM";
+import createTrackVM from "./createItemTrackVM";
 
 function createVoiceVM(model: Voice, context: TimelineContext): VoiceVM {
     const vm = new VoiceVM(
