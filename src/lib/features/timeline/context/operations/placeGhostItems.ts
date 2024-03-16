132441f3b728afdd02333e623094138e6001817a
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-16 15:29:06 +0100

    Fix placeGhostItems operation

--- src/lib/features/timeline/context/operations/placeGhostItems.ts
+++ src/lib/features/timeline/context/operations/placeGhostItems.ts
@@ -6,18 +6,26 @@ import {
 } from "../../../../architecture/state-hierarchy-utils";
 
 export default function placeGhostItems(context: TimelineContext) {
+    const selectedItems = Object.assign([], context.state.selectedItems);
+
     context.state.ghostPairs.forEach((pair) => {
         const oldTrack = getParent(pair[0]);
         const newTrack = getParent(pair[1]);
 
-        pair[0].state = pair[1].state;
-
         newTrack.cropItemsByInterval(pair[1].state.start, pair[1].state.end);
 
         removeChildren(oldTrack, pair[0]);
-        addChildren(newTrack, pair[0]);
+        addChildren(newTrack, pair[1]);
+
+        const index = selectedItems.indexOf(pair[0]);
+
+        if (index >= 0) {
+            selectedItems.splice(index, 1);
+            selectedItems.push(pair[1]);
+        }
     });
     context.state = {
         ghostPairs: [],
+        selectedItems: selectedItems,
     };
 }
