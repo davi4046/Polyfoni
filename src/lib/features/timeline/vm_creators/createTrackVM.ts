91b9fac3d638dcd98c6fa48249a563149ee8cae6
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-16 10:16:58 +0100

    Choose vm creator based on item type

--- src/lib/features/timeline/vm_creators/createTrackVM.ts
+++ src/lib/features/timeline/vm_creators/createTrackVM.ts
@@ -1,19 +1,32 @@
 import type TimelineContext from "../context/TimelineContext";
+import type Item from "../models/Item";
 import type Track from "../models/Track";
 import type { ItemTypes } from "../utils/ItemTypes";
+import type ItemVM from "../view_models/ItemVM";
 import TrackVM from "../view_models/TrackVM";
 
 import createItemVM from "./createItemVM";
 import createItemVM_ghost from "./createItemVM_ghost";
+import createNoteVM from "./createNoteVM";
 
-function createItemTrackVM<T extends keyof ItemTypes>(
+const itemTypeVMCreatorMap: {
+    [K in keyof ItemTypes]: (
+        model: Item<K>,
+        context: TimelineContext
+    ) => ItemVM;
+} = {
+    StringItem: createItemVM,
+    ChordItem: createItemVM,
+    NoteItem: createNoteVM,
+};
+
 export default function createTrackVM<T extends keyof ItemTypes>(
     model: Track<T>,
     context: TimelineContext
 ): TrackVM {
     const createItems = () => {
         const items = model.state.children.map((item) => {
-            return createItemVM(item, context);
+            return itemTypeVMCreatorMap[item.itemType](item, context);
         });
 
         const ghostItems = context.state.ghostPairs
