351ea6304fbb4555eddbf774a9d38c60076f5475
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-16 12:27:03 +0100

    Delete NoteVM.ts

--- src/lib/features/timeline/view_models/NoteVM.ts
+++
@@ -1,9 +0,0 @@
-import Model from "../../../architecture/Model";
-
-interface NoteVMState {
-    start: number;
-    end: number;
-    pitch: number;
-}
-
-export default class NoteVM extends Model<NoteVMState> {}
