6e00b616d56a711f56a26f9e8f88d916f9d95c76
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-17 10:49:52 +0100

    Delete Highlight.svelte

--- src/lib/features/timeline/visuals/Highlight.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let left: number;
-    export let width: number;
-</script>
-
-<div
-    class="absolute inset-y-0 bg-blue-500"
-    style="left:{left}px; width:{width}px;"
-/>
