321c516682eda6aae0fe0c22db6ddb5265905deb
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-20 09:26:17 +0100

    Improve change management logic

--- src/lib/features/generation/Generator.ts
+++ src/lib/features/generation/Generator.ts
@@ -1,38 +1,93 @@
 import type StateHierarchyWatcher from "../../architecture/StateHierarchyWatcher";
+import { countAncestors } from "../../architecture/state-hierarchy-utils";
+import type { ItemState } from "../timeline/models/Item";
 import type Timeline from "../timeline/models/Timeline";
+import type { TrackState } from "../timeline/models/Track";
 
 export default class Generator {
-    private _stateChanges: StateChange<any>[] = [];
+    private _itemChanges: ItemChange[] = [];
+    private _isHandlingChanges = false;
 
     constructor(watcher: StateHierarchyWatcher<Timeline>) {
         watcher.subscribe((obj, oldState) => {
-            const newState = obj.state;
-            this._stateChanges.push({ oldState, newState });
+            const objDepth = countAncestors(obj);
+            const newState = obj.state as any;
+            const change = { oldState, newState };
 
-            if (this._stateChanges.length === 1) {
+            switch (objDepth) {
+                // Track
+                case 3: {
+                    this._itemChanges.push(
+                        ...deriveItemChangesFromTrackChange(change)
+                    );
+                    break;
+                }
+                // Item
+                case 4: {
+                    this._itemChanges.push(change);
+                    break;
+                }
+            }
+
+            if (!this._isHandlingChanges) {
                 const handleNextChangeLoop = () => {
-                    const nextChange = this._stateChanges.shift();
+                    const nextChange = this._itemChanges.shift();
 
                     if (nextChange) {
                         this._handleChange(nextChange).then(
                             handleNextChangeLoop
                         );
                     } else {
-                        //else render output
+                        this._isHandlingChanges = false;
+                        //render output
                     }
                 };
+                this._isHandlingChanges = true;
                 handleNextChangeLoop();
             }
         });
     }
 
-    private async _handleChange(change: StateChange<any>) {}
+    private async _handleChange(change: ItemChange) {
+        if (change.oldState) this._clearItemStateEffect(change.oldState);
+        if (change.newState) this._applyItemStateEffect(change.newState);
+    }
+
+    private _clearItemStateEffect(itemState: ItemState<any>) {}
+
+    private _applyItemStateEffect(itemState: ItemState<any>) {}
 }
 
-type StateChange<TState extends object> = {
+type StateChange<TState> = {
     oldState: TState;
     newState: TState;
 };
+
+type ItemChange = StateChange<ItemState<any> | undefined>;
+
+function deriveItemChangesFromTrackChange(
+    change: StateChange<TrackState<any>>
+): ItemChange[] {
+    const removedItems = change.oldState.children.filter((child) => {
+        return !change.newState.children.includes(child);
+    });
+
+    const addedItems = change.newState.children.filter((child) => {
+        return !change.oldState.children.includes(child);
+    });
+
+    const itemChanges: ItemChange[] = [];
+
+    removedItems.forEach((item) => {
+        itemChanges.push({ oldState: item.state, newState: undefined });
+    });
+
+    addedItems.forEach((item) => {
+        itemChanges.push({ oldState: undefined, newState: item.state });
+    });
+
+    return itemChanges;
+}
 
 class NoteBuilder {
     constructor(
