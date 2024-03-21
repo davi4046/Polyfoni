6e5be0840298406525c989ef7999fbb1e82562b4
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-21 12:26:25 +0100

    Color chord items according to chord

--- src/lib/features/timeline/vm_creators/createItemVM.ts
+++ src/lib/features/timeline/vm_creators/createItemVM.ts
@@ -5,7 +5,11 @@ import type Item from "../models/Item";
 import EndHandleHandler from "../mouse_event_handlers/EndHandleHandler";
 import ItemHandler from "../mouse_event_handlers/ItemHandler";
 import StartHandleHandler from "../mouse_event_handlers/StartHandleHandler";
-import { itemTextFunctions, type ItemTypes } from "../utils/ItemTypes";
+import {
+    itemColorFunctions,
+    itemTextFunctions,
+    type ItemTypes,
+} from "../utils/ItemTypes";
 import ItemVM from "../view_models/ItemVM";
 import { mouseEventListener } from "../../../architecture/mouse-event-handling";
 
@@ -17,21 +21,32 @@ export default function createItemVM<T extends keyof ItemTypes>(
     const startHandleHandler = new StartHandleHandler(context, model);
     const endHandleHandler = new EndHandleHandler(context, model);
 
-    function createStyles() {
-        const styles: Record<string, string> = {
-            "background-color": chroma.hcl(0, 0, 80).css(),
-            "border-width": "2px",
-            "border-color": "black",
-            inset: "4px 0 4px 0",
-            padding: "0 8px 0 8px",
-        };
+    const baseInnerDivStyles: Record<string, string> = {
+        "border-width": "2px",
+        "border-color": "black",
+        inset: "4px 0 4px 0",
+        padding: "0 8px 0 8px",
+    };
+
+    const colorFunction = itemColorFunctions[model.itemType];
+
+    function createInnerDivStyles() {
+        const innerDivStyles = Object.assign({}, baseInnerDivStyles);
+
+        const customColor = colorFunction
+            ? colorFunction(model.state.content)
+            : undefined;
+
+        innerDivStyles["background-color"] = customColor
+            ? customColor.css()
+            : chroma.hcl(0, 0, 80).css();
 
         if (context.state.selectedItems.includes(model)) {
-            styles["outline-style"] = "solid";
-            styles["outline-color"] = chroma.hcl(240, 80, 80).css();
+            innerDivStyles["outline-style"] = "solid";
+            innerDivStyles["outline-color"] = chroma.hcl(240, 80, 80).css();
         }
 
-        return styles;
+        return innerDivStyles;
     }
 
     const vm = new ItemVM(
@@ -40,7 +55,7 @@ export default function createItemVM<T extends keyof ItemTypes>(
             end: model.state.end,
             text: itemTextFunctions[model.itemType](model.state.content),
 
-            innerDivStyles: createStyles(),
+            innerDivStyles: createInnerDivStyles(),
             handleStyles: {
                 "background-color": "black",
                 opacity: "0.25",
@@ -71,12 +86,13 @@ export default function createItemVM<T extends keyof ItemTypes>(
             start: model.state.start,
             end: model.state.end,
             text: itemTextFunctions[model.itemType](model.state.content),
+            innerDivStyles: createInnerDivStyles(),
         };
     });
 
     context.subscribe(() => {
         vm.state = {
-            innerDivStyles: createStyles(),
+            innerDivStyles: createInnerDivStyles(),
         };
     });
 
--- src/lib/features/timeline/vm_creators/createItemVM_ghost.ts
+++ src/lib/features/timeline/vm_creators/createItemVM_ghost.ts
@@ -2,27 +2,49 @@ import chroma from "chroma-js";
 
 import type TimelineContext from "../context/TimelineContext";
 import type Item from "../models/Item";
-import { type ItemTypes, itemTextFunctions } from "../utils/ItemTypes";
+import {
+    type ItemTypes,
+    itemTextFunctions,
+    itemColorFunctions,
+} from "../utils/ItemTypes";
 import ItemVM from "../view_models/ItemVM";
 
 export default function createItemVM_ghost<T extends keyof ItemTypes>(
     model: Item<T>,
     context: TimelineContext
 ): ItemVM {
+    const baseInnerDivStyles: Record<string, string> = {
+        "border-width": "2px",
+        "border-color": "black",
+        inset: "4px 0 4px 0",
+        padding: "0 8px 0 8px",
+        opacity: "0.75",
+    };
+
+    const colorFunction = itemColorFunctions[model.itemType];
+
+    function createInnerDivStyles() {
+        const innerDivStyles = Object.assign({}, baseInnerDivStyles);
+
+        const customColor = colorFunction
+            ? colorFunction(model.state.content)
+            : undefined;
+
+        innerDivStyles["background-color"] = customColor
+            ? customColor.css()
+            : chroma.hcl(0, 0, 80).css();
+
+        return innerDivStyles;
+    }
+
     const vm = new ItemVM(
         {
             start: model.state.start,
             end: model.state.end,
             text: itemTextFunctions[model.itemType](model.state.content),
 
-            innerDivStyles: {
-                "background-color": chroma.hcl(0, 0, 80).css(),
-                opacity: "0.75",
-                "border-width": "2px",
-                "border-color": "black",
-                inset: "4px 0 4px 0",
-                padding: "0 8px 0 8px",
-            },
+            innerDivStyles: createInnerDivStyles(),
+
             handleStyles: {
                 "background-color": "black",
                 opacity: "0.25",
@@ -36,6 +58,7 @@ export default function createItemVM_ghost<T extends keyof ItemTypes>(
             start: model.state.start,
             end: model.state.end,
             text: itemTextFunctions[model.itemType](model.state.content),
+            innerDivStyles: createInnerDivStyles(),
         };
     });
 
