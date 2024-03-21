1a1856304e2437a0bd23ef2e92d2ec335b5f9e00
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-21 15:06:34 +0100

    Introduce TimelinePlayer

---
+++ src/lib/features/playback/TimelinePlayer.ts
@@ -0,0 +1,97 @@
+import Stateful from "../../architecture/Stateful";
+import {
+    getChildren,
+    getIndex,
+} from "../../architecture/state-hierarchy-utils";
+import type Item from "../timeline/models/Item";
+import type Timeline from "../timeline/models/Timeline";
+import type Voice from "../timeline/models/Voice";
+import { midiPlayer } from "../timeline/utils/midiPlayer";
+import { trackTypeToIndex } from "../timeline/utils/track-config";
+
+interface TimelinePlayerState {
+    playbackPosition: number;
+    isPlaying: boolean;
+}
+
+export default class TimelinePlayer extends Stateful<TimelinePlayerState> {
+    constructor(timeline: Timeline) {
+        super({ playbackPosition: 0, isPlaying: false });
+        this._timeline = timeline;
+    }
+
+    private _timeline: Timeline;
+    private _bpm = 60;
+    private _interval: NodeJS.Timeout | undefined;
+
+    startPlayback() {
+        if (this.state.isPlaying) this.pausePlayback();
+
+        const voices = getChildren(getChildren(this._timeline)[1]);
+
+        voices.forEach((voice) => {
+            midiPlayer.allSoundOff(getIndex(voice));
+            this._schedulePlayback(voice);
+        });
+
+        const intervalDuration = 10;
+
+        this._interval = setInterval(() => {
+            this.state = {
+                playbackPosition:
+                    this.state.playbackPosition +
+                    this._bpm / 60000 / intervalDuration,
+            };
+        }, intervalDuration);
+
+        this.state = {
+            isPlaying: true,
+        };
+    }
+
+    pausePlayback() {
+        clearInterval(this._interval);
+
+        this._voiceTimeouts.forEach((timeouts, voice) => {
+            timeouts.forEach(clearTimeout);
+            midiPlayer.allSoundOff(getIndex(voice));
+        });
+
+        this._voiceTimeouts.clear();
+
+        this.state = {
+            isPlaying: false,
+        };
+    }
+
+    private _voiceTimeouts = new Map<Voice, NodeJS.Timeout[]>();
+
+    private _schedulePlayback(voice: Voice) {
+        const outputTrack = getChildren(voice)[trackTypeToIndex("output")];
+
+        const timeouts: NodeJS.Timeout[] = [];
+
+        getChildren(outputTrack).forEach((note: Item<"NoteItem">) => {
+            if (note.state.start < this.state.playbackPosition) return;
+
+            const relativeStart =
+                note.state.start - this.state.playbackPosition;
+            const relativeEnd = note.state.end - this.state.playbackPosition;
+
+            const relativeStartMS = (relativeStart / this._bpm) * 60000;
+            const relativeEndMS = (relativeEnd / this._bpm) * 60000;
+
+            const startTimeout = setTimeout(() => {
+                midiPlayer.noteOn(getIndex(voice), note.state.content, 100);
+            }, relativeStartMS);
+
+            const endTimeout = setTimeout(() => {
+                midiPlayer.noteOff(getIndex(voice), note.state.content);
+            }, relativeEndMS);
+
+            timeouts.push(startTimeout, endTimeout);
+        });
+
+        this._voiceTimeouts.set(voice, timeouts);
+    }
+}
--- src/lib/features/timeline/context/TimelineContext.ts
+++ src/lib/features/timeline/context/TimelineContext.ts
@@ -1,6 +1,7 @@
 import type Highlight from "../models/Highlight";
 import type Item from "../models/Item";
 import type Timeline from "../models/Timeline";
+import TimelinePlayer from "../../playback/TimelinePlayer";
 import Stateful from "../../../architecture/Stateful";
 
 interface TimelineContextState {
@@ -17,5 +18,9 @@ export default class TimelineContext extends Stateful<TimelineContextState> {
             ghostPairs: [],
             selectedItems: [],
         });
+
+        this.player = new TimelinePlayer(timeline);
     }
+
+    player: TimelinePlayer;
 }
