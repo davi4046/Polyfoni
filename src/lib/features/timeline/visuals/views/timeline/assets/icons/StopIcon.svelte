64018725ceac9dfecbba4d03938192e0b37811d9
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-22 07:46:03 +0100

    Create play, pause, and stop icons

---
+++ src/lib/features/timeline/visuals/views/timeline/assets/icons/StopIcon.svelte
@@ -0,0 +1,10 @@
+<svg
+    class="h-full"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 -960 960 960"
+    fill="currentColor"
+>
+    <path d="M240-240v-480h480v480H240Z" />
+</svg>
+
+<!-- https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Astop%3AFILL%401%3Bwght%40400%3BGRAD%400%3Bopsz%4024 -->
