b99db5d91975a0bb83acd469fbb6598b96efb25f
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-27 09:05:37 +0100

    Add tempo changes when creating MIDI file

--- src/lib/features/export/createMidiFileFromTimeline.ts
+++ src/lib/features/export/createMidiFileFromTimeline.ts
@@ -3,11 +3,12 @@ import * as midi from "@perry-rylance/midi";
 import { getChildren } from "../../architecture/state-hierarchy-utils";
 import type Timeline from "../timeline/models/Timeline";
 import type Track from "../timeline/models/Track";
+import { deriveTempoChangesFromItems } from "../timeline/utils/tempo-utils";
 import { trackTypeToIndex } from "../timeline/utils/track-config";
 
-export default function createMidiFileFromTimeline(
+export default async function createMidiFileFromTimeline(
     timeline: Timeline
-): ArrayBuffer {
+): Promise<ArrayBuffer> {
     const voices = getChildren(getChildren(timeline)[1]);
 
     const outputTracks: Track<"NoteItem">[] = voices.map(
@@ -16,14 +17,38 @@ export default function createMidiFileFromTimeline(
 
     const midiFile = new midi.File();
 
+    const eventTrack = new midi.Track();
+
+    const timeSignatureEvent = new midi.TimeSignatureEvent();
+    timeSignatureEvent.numerator = 4;
+    timeSignatureEvent.denominator = 4;
+    eventTrack.events.push(timeSignatureEvent);
+
+    const tempoItems = getChildren(timeline.tempoTrack).slice();
+    const tempoChanges = await deriveTempoChangesFromItems(tempoItems, 60);
+
+    for (let i = 0; i < tempoChanges.length; i++) {
+        const prevChange = tempoChanges[i - 1];
+        const currChange = tempoChanges[i];
+
+        const tempoEvent = new midi.SetTempoEvent();
+
+        tempoEvent.delta = prevChange
+            ? currChange.beat - prevChange.beat
+            : currChange.beat;
+        tempoEvent.delta *= 480;
+
+        tempoEvent.bpm = currChange.tempo;
+
+        eventTrack.events.push(tempoEvent);
+    }
+
+    eventTrack.events.push(new midi.EndOfTrackEvent());
+
+    midiFile.tracks.push(eventTrack);
+
     outputTracks.forEach((track) => {
         const midiTrack = new midi.Track();
-
-        const timeSignatureEvent = new midi.TimeSignatureEvent();
-        timeSignatureEvent.numerator = 4;
-        timeSignatureEvent.denominator = 4;
-
-        midiTrack.events.push(timeSignatureEvent);
 
         const notes = getChildren(track).slice();
         notes.sort((a, b) => a.state.start - b.state.start);
@@ -35,13 +60,13 @@ export default function createMidiFileFromTimeline(
             const noteOnEvent = new midi.NoteOnEvent();
             const noteOffEvent = new midi.NoteOffEvent();
 
-            noteOnEvent.delta = prevNote
+            const startDeltaBeat = prevNote
                 ? currNote.state.start - prevNote.state.end
                 : currNote.state.start;
-            noteOffEvent.delta = currNote.state.end - currNote.state.start;
+            const endDeltaBeat = currNote.state.end - currNote.state.start;
 
-            noteOnEvent.delta *= 480;
-            noteOffEvent.delta *= 480;
+            noteOnEvent.delta = Math.round(startDeltaBeat * 480);
+            noteOffEvent.delta = Math.round(endDeltaBeat * 480);
 
             noteOnEvent.key = currNote.state.content;
             noteOffEvent.key = currNote.state.content;
