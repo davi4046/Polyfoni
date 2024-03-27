7ec5ee1d5e98a817b6faa1a652bd27108864938a
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-27 09:36:54 +0100

    Replace IdProvider with lodash function

--- src/lib/architecture/Model.ts
+++ src/lib/architecture/Model.ts
@@ -1,13 +1,11 @@
-import IdProvider from "./IdProvider";
+import { uniqueId } from "lodash";
 
 import Stateful from "./Stateful";
-
-const modelIdProvider = new IdProvider();
 
 export default class Model<TState extends object> extends Stateful<TState> {
     constructor(
         state: TState,
-        readonly id = modelIdProvider.generateId()
+        readonly id = uniqueId()
     ) {
         super(state);
     }
