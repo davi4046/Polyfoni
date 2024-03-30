514088082dd724e786bee995b172c2d2d826b4e9
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-03-30 09:03:43 +0100

    Remove unused imports

--- src/timeline/features/generation/Generator.ts
+++ src/timeline/features/generation/Generator.ts
@@ -1,7 +1,5 @@
 import { invoke } from "@tauri-apps/api";
 
-import { isBoolean, range } from "lodash";
-
 import StateHierarchyWatcher from "../../../architecture/StateHierarchyWatcher";
 import type Stateful from "../../../architecture/Stateful";
 import {
