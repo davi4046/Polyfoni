9995e0742b6613af1fc48f187f3ed22f52b25017
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-04-03 12:10:07 +0200

    Reimplement multiselect of start grips

--- src/timeline/user_interface/event_handlers/StartGripHandler.ts
+++ src/timeline/user_interface/event_handlers/StartGripHandler.ts
@@ -1,10 +1,11 @@
-import { clamp } from "lodash";
+import { clamp, clone } from "lodash";
 
 import type TimelineContext from "../context/TimelineContext";
 import cropItemsByInterval from "../../utils/cropItemsByInterval";
 import type { GlobalEventHandler } from "../../../architecture/GlobalEventListener";
 import {
     getChildren,
+    getGrandparent,
     getParent,
 } from "../../../architecture/state-hierarchy-utils";
 import type Item from "../../models/item/Item";
@@ -45,16 +46,27 @@ export default class StartGripHandler implements GlobalEventHandler {
             getBeatAtClientX(this.context.timeline, event.clientX)
         );
 
-        const clampedBeat = clamp(hoveredBeat, 0, this.item.state.end - 1);
+        const grippedItems = Array.from(this.context.state.grips.keys());
+
+        const minEnd = grippedItems.reduce(
+            (minEnd, item) =>
+                item.state.end < minEnd ? item.state.end : minEnd,
+            Number.MAX_SAFE_INTEGER
+        );
+
+        const clampedBeat = clamp(hoveredBeat, 0, minEnd - 1);
 
         if (clampedBeat === this._prevBeat) return;
 
         this._prevBeat = clampedBeat;
 
         this.context.state = {
-            grips: new Map([
-                [this.item, { property: "start", value: clampedBeat }],
-            ]),
+            grips: new Map(
+                grippedItems.map((item) => [
+                    item,
+                    { property: "start", value: clampedBeat },
+                ])
+            ),
         };
     }
 
@@ -77,6 +89,36 @@ export default class StartGripHandler implements GlobalEventHandler {
 
     handleKeyDown(event: KeyboardEvent) {
         if (event.key !== "Shift") return;
+
+        const grippedItems = Array.from(this.context.state.grips.keys());
+        const grippedPoint = this._prevBeat
+            ? this._prevBeat
+            : this.item.state.start;
+
+        const tracks = getChildren(getGrandparent(this.item))
+            .filter((track) => track.itemType !== "NoteItem")
+            .filter((track) => {
+                return !grippedItems.some((item) => getParent(item) === track);
+            }); // Only search tracks where there is no gripped item
+
+        const matchStartItems = tracks
+            .flatMap((track) => getChildren(track))
+            .filter((item) => {
+                return item.state.start === grippedPoint;
+            });
+
+        const newGrips = clone(this.context.state.grips);
+
+        matchStartItems.forEach((item) => {
+            newGrips.set(item, {
+                property: "start",
+                value: item.state.start,
+            });
+        });
+
+        this.context.state = {
+            grips: newGrips,
+        };
     }
 }
 
