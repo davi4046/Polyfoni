e7f2fe5e7ec802248c9e160a5be0511d12f0b391
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-04-03 13:40:37 +0200

    Create TimelineHistory.ts

---
+++ src/timeline/features/undo-redo/TimelineHistory.ts
@@ -0,0 +1,83 @@
+import StateHierarchyWatcher from "../../../architecture/StateHierarchyWatcher";
+import type Stateful from "../../../architecture/Stateful";
+import type Timeline from "../../models/timeline/Timeline";
+
+export default class TimelineHistory {
+    constructor(timeline: Timeline) {
+        const watcher = new StateHierarchyWatcher(timeline);
+
+        watcher.subscribe((obj, oldState) => {
+            if (!this._currAction) return;
+
+            const newState = obj.state;
+
+            this._currAction.changes.push({ obj, oldState, newState });
+        });
+    }
+
+    private _currAction?: Action;
+    private _undoableActions: Action[] = [];
+    private _redoableActions: Action[] = [];
+
+    startAction(title: string) {
+        if (this._currAction) {
+            console.warn(
+                "Tried to start an action" +
+                    "but the current action" +
+                    "has not been ended yet"
+            );
+            return;
+        }
+        this._currAction = new Action(title);
+    }
+
+    endAction() {
+        if (!this._currAction) {
+            console.warn("Tried to end action but no action has been started");
+            return;
+        }
+        this._undoableActions.push(this._currAction);
+        this._currAction = undefined;
+
+        console.log(this._undoableActions);
+    }
+
+    undoAction() {
+        const lastAction = this._undoableActions.pop();
+
+        if (!lastAction) return;
+
+        lastAction.changes.reverse().forEach(({ obj, oldState, newState }) => {
+            obj.state = oldState;
+        });
+
+        this._redoableActions.push(lastAction);
+    }
+
+    redoAction() {
+        const lastAction = this._redoableActions.pop();
+
+        if (!lastAction) return;
+
+        lastAction.changes.forEach(({ obj, oldState, newState }) => {
+            obj.state = newState;
+        });
+
+        this._undoableActions.push(lastAction);
+    }
+
+    private _revertActionChanges(action: Action) {}
+}
+
+class Action {
+    constructor(
+        readonly title: string,
+        readonly changes: StateChange<any>[] = []
+    ) {}
+}
+
+type StateChange<TState extends object> = {
+    obj: Stateful<TState>;
+    oldState: TState | undefined;
+    newState: TState | undefined;
+};
