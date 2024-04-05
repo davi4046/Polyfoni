f49e41ba1eda9f75d1aac3b127ca1f15fc17ba5d
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-04-05 08:02:30 +0200

    Simplify typing

--- src/timeline/user_interface/context/TimelineContext.ts
+++ src/timeline/user_interface/context/TimelineContext.ts
@@ -2,28 +2,18 @@ import Stateful from "../../../architecture/Stateful";
 import TimelinePlayer from "../../features/playback/TimelinePlayer";
 import TimelineHistory from "../../features/undo-redo/TimelineHistory";
 import type Highlight from "../../models/highlight/Highlight";
-import type { ItemState } from "../../models/item/Item";
 import type Item from "../../models/item/Item";
-import type { ItemTypes } from "../../models/item/ItemTypes";
 import type Timeline from "../../models/timeline/Timeline";
 
 interface TimelineContextState {
     highlights: Highlight<any>[];
     selectedItems: Item<any>[];
     ghostPairs: [Item<any>, Item<any>][];
-    grips: Map<Item<any>, ItemKeyValuePair<any, "start" | "end">>;
+    grips: Map<Item<any>, { property: "start" | "end"; value: number }>;
     clipboard: Item<any>[];
 
     editItem?: Item<any>;
 }
-
-type ItemKeyValuePair<
-    T extends keyof ItemTypes,
-    U extends keyof ItemState<T>,
-> = {
-    property: U;
-    value: ItemState<T>[U];
-};
 
 export default class TimelineContext extends Stateful<TimelineContextState> {
     constructor(readonly timeline: Timeline) {
