6f948983b31fd8bb0f60981252e762e75604216d
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-04-10 09:25:18 +0200

    Clean createVoiceVM.ts

--- src/timeline/user_interface/vm_creators/voice_vm/createVoiceVM.ts
+++ src/timeline/user_interface/vm_creators/voice_vm/createVoiceVM.ts
@@ -1,10 +1,8 @@
+import createTrackGroupVM from "../track_group_vm/createTrackGroupVM";
 import type TimelineContext from "../../context/TimelineContext";
-import type TrackGroupVM from "../../view_models/TrackGroupVM";
 import VoiceVM from "../../view_models/VoiceVM";
 import { getChildren } from "../../../../architecture/state-hierarchy-utils";
 import type Voice from "../../../models/voice/Voice";
-
-import createTrackGroupVM from "../track_group_vm/createTrackGroupVM";
 
 export default function createVoiceVM(
     model: Voice,
