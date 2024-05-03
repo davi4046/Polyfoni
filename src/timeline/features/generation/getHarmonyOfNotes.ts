c6805d7abd0b1f5ec050cbc3b33a94bded7339ba
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-05-03 12:48:07 +0200

    Consider only framework notes when analysing harmony

--- src/timeline/features/generation/getHarmonyOfNotes.ts
+++ src/timeline/features/generation/getHarmonyOfNotes.ts
@@ -5,10 +5,14 @@ import type Item from "../../models/item/Item";
 export default function getHarmonyOfNotes(
     notes: Item<"NoteItem">[]
 ): Chord | undefined {
+    notes = notes.filter((note) => note.state.content.type !== "Decoration");
+
     if (notes.length === 0) return;
 
     const rootMidiValue = notes.reduce((minPitch, note) => {
-        return note.state.content < minPitch ? note.state.content : minPitch;
+        return note.state.content.pitch < minPitch
+            ? note.state.content.pitch
+            : minPitch;
     }, Number.MAX_SAFE_INTEGER);
 
     const root = Object.values(pitchNames)[(rootMidiValue + 3) % 12];
@@ -17,7 +21,7 @@ export default function getHarmonyOfNotes(
         pitchNames.map((pitch) => {
             const midiValue = (pitchNames.indexOf(pitch) + 9) % 12;
             const isPresent = notes.some(
-                (note) => note.state.content % 12 === midiValue
+                (note) => note.state.content.pitch % 12 === midiValue
             );
             return [pitch, isPresent];
         })
