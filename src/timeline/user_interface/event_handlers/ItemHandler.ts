8d021dc60cbb4758ce0b59d1cf24c1e1232fbdd6
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-05-23 10:22:00 +0200

    Fix import

--- src/timeline/user_interface/event_handlers/ItemHandler.ts
+++ src/timeline/user_interface/event_handlers/ItemHandler.ts
@@ -1,4 +1,4 @@
-import { clamp } from "lodash";
+import { clamp } from "lodash-es";
 
 import type TimelineContext from "../context/TimelineContext";
 import placeGhostItems from "../context/operations/placeGhostItems";
