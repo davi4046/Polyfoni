f6d7c1b4ccb33443287e4f1befe6591546921fa7
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-06-07 09:14:03 +0200

    Change target to ES2015 and polyfill with core-js

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import App from "./App.svelte";
 import "./styles.css";
 
 import "./utils/html_classes/adjust-width-to-height";
+import "core-js";
 
 const app = new App({
     target: document.getElementById("app")!,
